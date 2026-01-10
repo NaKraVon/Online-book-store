0d23db77d9503348105aab3eae2f29c998aebadb
mr-Edoc <mr.edoc.work@gmail.com>
2026-01-11 01:49:40 +0200

    Ver1/Created model, repo and service for book model

--- src/main/java/com/example/demo/OnlineBookStoreApplication.java
+++ src/main/java/com/example/demo/OnlineBookStoreApplication.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import com.example.demo.model.Book;
+import com.example.demo.service.BookService;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
+    @Autowired
+    private BookService bookService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineBookStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setTitle("Book 1");
+            book.setAuthor("Author 1");
+            book.setIsbn("ISBN 1");
+            book.setPrice(BigDecimal.valueOf(199));
+
+            bookService.save(book);
+            bookService.findAll().forEach(System.out::println);
+        };
+    }
 }
