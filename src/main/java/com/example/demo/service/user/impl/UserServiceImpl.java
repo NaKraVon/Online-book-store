3d2675c116dc96d498eb4412be05c3fb68bcd7a7
mr-Edoc <mr.edoc.work@gmail.com>
2026-01-27 18:40:22 +0200

    Completed second part of Security: Added spring-boot-starter-security, SecurityConfig, Role model, RoleRepository and Liquibase changesets. Implement UserDetails and UserDetailsService. Used @PreAuthorize for endpoints.

--- src/main/java/com/example/demo/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/user/impl/UserServiceImpl.java
@@ -2,23 +2,31 @@ package com.example.demo.service.user.impl;
 
 import com.example.demo.dto.user.UserRegistrationRequestDto;
 import com.example.demo.dto.user.UserResponseDto;
+import com.example.demo.exception.EntityNotFoundException;
 import com.example.demo.exception.RegistrationException;
 import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.Role;
 import com.example.demo.model.User;
+import com.example.demo.model.enums.RoleName;
+import com.example.demo.repository.role.RoleRepository;
 import com.example.demo.repository.user.UserRepository;
 import com.example.demo.service.user.UserService;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
-    @Transactional
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
             throws RegistrationException {
         if (userRepository.existsByEmail(requestDto.getEmail())) {
@@ -26,10 +34,13 @@ public class UserServiceImpl implements UserService {
                     "Email already exists: " + requestDto.getEmail()
             );
         }
-
         User user = userMapper.toUser(requestDto);
-        User savedUser = userRepository.save(user);
-
-        return userMapper.toUserResponse(savedUser);
+        user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        user.setShippingAddress(requestDto.getShippingAddress());
+        Role userRole = roleRepository.findByRoleName(RoleName.USER)
+                .orElseThrow(() -> new EntityNotFoundException("Can't find role by name: "
+                        + RoleName.USER.name()));
+        user.setRoles(Set.of(userRole));
+        return userMapper.toUserResponse(userRepository.save(user));
     }
 }
