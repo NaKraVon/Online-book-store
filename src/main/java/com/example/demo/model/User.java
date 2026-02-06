8ba10490adcc4a438f5a9752434524205bdc8ebc
mr-Edoc <mr.edoc.work@gmail.com>
2026-02-07 00:03:29 +0200

    Fixed checkstyle problem

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -9,7 +10,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.FetchType;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
