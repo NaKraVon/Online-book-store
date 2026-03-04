a275428733e28613020dfc938b81c16454b9fa74
mr-Edoc <mr.edoc.work@gmail.com>
2026-03-04 21:38:17 +0200

    Added Order model

---
+++ src/main/java/com/example/demo/mapper/OrderMapper.java
@@ -0,0 +1,13 @@
+package com.example.demo.mapper;
+
+import com.example.demo.config.MapperConfig;
+import com.example.demo.dto.order.OrderResponseDto;
+import com.example.demo.model.Order;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapperConfig.class, uses = OrderItemMapper.class)
+public interface OrderMapper {
+    @Mapping(source = "user.id", target = "userId")
+    OrderResponseDto toOrderResponseDto(Order order);
+}
--- src/main/java/com/example/demo/repository/cartitem/CartItemRepository.java
+++ src/main/java/com/example/demo/repository/cartitem/CartItemRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.repository.cartitem;
 
 import com.example.demo.model.CartItem;
+import java.util.Optional;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Optional<Set<CartItem>> getAllCartItemsByShoppingCartId(Long id);
 }
