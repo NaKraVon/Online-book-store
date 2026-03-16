38d139601b5760f9c929f5c3fd679dcac953b3d2
mr-Edoc <mr.edoc.work@gmail.com>
2026-03-16 14:06:45 +0200

    Created CategoryControllerIntegrationTest and update checkstyle in BookControllerIntegrationTest

--- src/test/java/com/example/demo/controller/integration/CategoryControllerIntegrationTest.java
+++ src/test/java/com/example/demo/controller/integration/CategoryControllerIntegrationTest.java
@@ -9,7 +9,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.demo.TestDataHelper;
-import com.example.demo.dto.book.CreateBookRequestDto;
+import com.example.demo.dto.category.CategoryRequestDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -24,7 +24,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
-public class BookControllerIntegrationTest {
+public class CategoryControllerIntegrationTest {
 
     private static MockMvc mockMvc;
 
@@ -40,94 +40,92 @@ public class BookControllerIntegrationTest {
                 .build();
     }
 
+
     @Test
-    @DisplayName("Get book by ID - returns existing book")
+    @DisplayName("Given valid category request, create a new category and return 201 Created")
+    @WithMockUser(roles = "ADMIN")
+    @Sql(scripts = "classpath:database/book-category/remove-all.sql",
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void createCategory_ValidRequest_ReturnsCreatedCategory() throws Exception {
+        CategoryRequestDto categoryRequestDto = testDataHelper.createCategoryRequestDto();
+
+        String jsonRequest = objectMapper.writeValueAsString(categoryRequestDto);
+
+        mockMvc.perform(post("/categories")
+                        .content(jsonRequest)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").exists());
+
+    }
+
+    @Test
+    @DisplayName("Get category by ID - returns existing category")
     @WithMockUser(roles = "USER")
     @Sql(scripts = "classpath:database/book-category/add-books-and-categories.sql",
             executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "classpath:database/book-category/remove-all.sql",
             executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    public void getBookById_ValidId_ReturnsBook() throws Exception {
-        Long bookId = 1L;
+    public void getCategoryById_ValidId_ReturnsCategory() throws Exception {
+        Long categoryId = 1L;
 
-        mockMvc.perform(get("/books/{id}", bookId)
+        mockMvc.perform(get("/categories/{id}", categoryId)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title").value("Test Book 1"));
+                .andExpect(jsonPath("$.name").value("Fiction"));
     }
 
     @Test
-    @DisplayName("Given valid book request, create a new book and return 201 Created")
+    @DisplayName("Get all categories - returns page")
+    @WithMockUser(roles = "USER")
+    @Sql(scripts = "classpath:database/book-category/add-books-and-categories.sql",
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "classpath:database/book-category/remove-all.sql",
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void getAllCategories_ValidId_ReturnsPage() throws Exception {
+
+        mockMvc.perform(get("/categories")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].id").value(1L))
+                .andExpect(jsonPath("$.content[0].name").value("Fiction"));
+    }
+
+    @Test
+    @DisplayName("Update category - returns 200 Ok for Admin")
+    @WithMockUser(roles = "ADMIN")
+    @Sql(scripts = "classpath:database/book-category/add-books-and-categories.sql",
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "classpath:database/book-category/remove-all.sql",
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void updateCategory_ValidId_ReturnsOk() throws Exception {
+        Long categoryId = 1L;
+        CategoryRequestDto categoryRequestDto = testDataHelper.createCategoryRequestDto();
+
+        String jsonRequest = objectMapper.writeValueAsString(categoryRequestDto);
+
+        mockMvc.perform(put("/categories/{id}", categoryId)
+                        .content(jsonRequest)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(categoryId))
+                .andExpect(jsonPath("$.name").value("Fiction"));
+
+
+    }
+
+    @Test
+    @DisplayName("Get all categories - returns page")
     @WithMockUser(roles = "ADMIN")
     @Sql(scripts = "classpath:database/book-category/add-category.sql",
             executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "classpath:database/book-category/remove-all.sql",
             executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    public void createBook_ValidRequest_ReturnsCreatedBook() throws Exception {
-        CreateBookRequestDto requestDto = testDataHelper.createBookRequestDto();
+    public void deleteCategory_ValidId_ReturnsNoContent() throws Exception {
+        Long categoryId = 1L;
 
-        String jsonRequest = objectMapper.writeValueAsString(requestDto);
-
-        mockMvc.perform(post("/books")
-                        .content(jsonRequest)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.title").exists())
-                .andExpect(jsonPath("$.author").exists());
-    }
-
-
-    @Test
-    @DisplayName("Get all books - returns page of books")
-    @WithMockUser(roles = "USER")
-    @Sql(scripts = "classpath:database/book-category/add-books-and-categories.sql",
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = "classpath:database/book-category/remove-all.sql",
-            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    public void getAllBooks_ValidRequest_ReturnsPage() throws Exception {
-
-        mockMvc.perform(get("/books")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content[0].id").value(1L))
-                .andExpect(jsonPath("$.content[0].title").value("Test Book 1"));;
-
-    }
-
-    @Test
-    @DisplayName("Delete book - returns 204 No Content for Admin")
-    @WithMockUser(roles = "ADMIN")
-    @Sql(scripts = "classpath:database/book-category/add-books-and-categories.sql",
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = "classpath:database/book-category/remove-all.sql",
-            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    public void deleteBook_ValidId_ReturnsNoContent() throws Exception {
-        Long bookId = 1L;
-
-        mockMvc.perform(delete("/books/{id}", bookId))
+        mockMvc.perform(delete("/categories/{id}", categoryId))
                 .andExpect(status().isNoContent());
-    }
-
-    @Test
-    @DisplayName("Update book - returns 200 Ok for Admin")
-    @WithMockUser(roles = "ADMIN")
-    @Sql(scripts = "classpath:database/book-category/add-books-and-categories.sql",
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = "classpath:database/book-category/remove-all.sql",
-            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    public void updateBook_ValidId_ReturnsOk() throws Exception {
-        Long bookId = 1L;
-        CreateBookRequestDto requestDto = testDataHelper.createBookRequestDto();
-
-        String jsonRequest = objectMapper.writeValueAsString(requestDto);
-
-        mockMvc.perform(put("/books/{id}", bookId)
-                        .content(jsonRequest)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(bookId))
-                .andExpect(jsonPath("$.title").value("New Book"))
-                .andExpect(jsonPath("$.author").value("John Doe"));
     }
 }
